2160d0d035a912d36bdd907779b45d2b72dabcd7
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-14 18:30:18 +0200

    added layout

---
+++ resources/views/layouts/footer.view.php
@@ -0,0 +1,9 @@
+<footer class="h-20">
+    <div class="sm:px-20 divide-y-2 divide-gray-900 divide-solid">
+        <div></div>
+        <div></div>
+    </div>
+    <div class="text-center mt-8">
+        Scandiweb Test assignment
+    </div>
+</footer>
